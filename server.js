f5909c682356c8eab25c12a2b84f9d0207ea4978
“dev-juanda01” <penalozahernandezjuandavid03@gmail.com>
2025-11-25 02:00:15 -0500

    feat: webhook meta api official config to managemente interactive messages

--- server.js
+++ server.js
@@ -4,11 +4,12 @@ import cors from "cors";
 import path from "path";
 import { fileURLToPath } from "url";
 
-import swaggerUi from 'swagger-ui-express';
-import swaggerSpec from './src/config/swagger.js';
+import swaggerUi from "swagger-ui-express";
+import swaggerSpec from "./src/config/swagger.js";
 
 import logger from "./src/utils/logger.js";
 import sessionRoutes from "./src/api/routes/session.routes.js";
+import metaRoutes from "./src/api/routes/meta.routes.js";
 import { initializeDirectories } from "./src/utils/init.js";
 import SessionManager from "./src/services/SessionManager.js";
 import { bannerBaileysServerPro } from "./src/utils/banner.js";
@@ -28,11 +29,14 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use("/api/sessions", sessionRoutes);
+app.use("/api/meta", metaRoutes);
 
 app.listen(PORT, () => {
     logger.info(banner);
     logger.info(`✅ Servidor escuchando en http://localhost:${PORT}`);
-    logger.info(`📕 Documentación disponible en http://localhost:${PORT}/api-docs`);
+    logger.info(
+        `📕 Documentación disponible en http://localhost:${PORT}/api-docs`
+    );
 });
